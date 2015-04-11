e411855627e215bae5608b40c4450198bc5dd1fb
seanokeeffe <seanokeeffe28@gmail.com>
2015-04-10 20:45:33 -0700

    Initial commit
    Xcode project

---
+++ WatchAid/WatchAid/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WatchAid
+//
+//  Created by Sean on 4/10/15.
+//  Copyright (c) 2015 Sean O'Keeffe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
